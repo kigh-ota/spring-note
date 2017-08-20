1bf507d288e23345980b76a9f015b964458f8050
kaiichiro-ota <kaiichiro-ota@cybozu.co.jp>
2017-08-20 23:18:24 +0900

    Try to fix running tests on CircleCI

--- src/test/java/note/NoteApplicationTests.java
+++ src/test/java/note/NoteApplicationTests.java
@@ -2,10 +2,11 @@ package note;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class NoteApplicationTests {
 
 	@Test
